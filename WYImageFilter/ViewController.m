97dca29351391049c4455fafe41922051b529713
wangyan <wangyan1@wanglibank.com>
2016-05-23 18:18:38 +0800

    滤镜拍照

--- WYImageFilter/ViewController.m
+++ WYImageFilter/ViewController.m
@@ -6,22 +6,54 @@
 //  Copyright © 2016年 wanglijinrong. All rights reserved.
 //
 
+#import "GPUImage.h"
 #import "ViewController.h"
+#import "GPUImageBeautifyFilter.h"
 
-@interface ViewController ()
+#define VIDEO_RECT CGRectMake(40, 64, 240, 320)
 
-@end
 
 @implementation ViewController
+{
+    GPUImageStillCamera *_stillCamera;  /// 照相机
+    GPUImageView        *_frameView;    /// 图像框
+    GPUImageBeautifyFilter *_beautifyFilter; /// 美颜滤镜
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self beautifyVideo];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)beautifyVideo {
+    _stillCamera = [[GPUImageStillCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionFront];
+    _stillCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    
+    _beautifyFilter = [GPUImageBeautifyFilter new];
+    _frameView      = [[GPUImageView alloc] initWithFrame:VIDEO_RECT];
+    
+    [_beautifyFilter addTarget:_frameView];
+    [_stillCamera addTarget:_beautifyFilter];
+    
+    [self.view addSubview:_frameView];
+    [_stillCamera startCameraCapture];
+}
+
+
+- (IBAction)takePhotoBtnClick {
+    [_stillCamera capturePhotoAsPNGProcessedUpToFilter:_beautifyFilter withCompletionHandler:^(NSData *processedPNG, NSError *error) {
+        if (error) NSLog(@"error = %@", error);
+        
+        UIImage *image = [UIImage imageWithData:processedPNG];
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:VIDEO_RECT];
+        imageView.image = image;
+        [self.view addSubview:imageView];
+        
+        NSLog(@"image = %@", image);
+        NSLog(@"currentThread = %@", [NSThread currentThread]);
+    }];
 }
 
 @end
---
+++ iOSInjectionProject/build/InjectionBundle.build/Debug-iphoneos/InjectionBundle.build/Objects-normal/arm64/BundleContents.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/wlb/Desktop/网利金融/网利金融/IOS学习/美颜滤镜/WYImageFilter/iOSInjectionProject/BundleContents.m \
+  /tmp/injectionforxcode/BundleInjection.h \
+  /tmp/injectionforxcode/BundleInterface.h \
+  /Users/wlb/Desktop/网利金融/网利金融/IOS学习/美颜滤镜/WYImageFilter/iOSInjectionProject/BundleContents.h
